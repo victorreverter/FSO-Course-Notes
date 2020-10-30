49dc3cd65dfcb965fcf759c9e9dde4d06a1d257b
victorreverter <victorreverter@gmail.com>
2020-10-30 03:02:27 +0100

    first Commit

---
+++ notes-app-backend/build/precache-manifest.ebf55591a0b8449d170333347275a434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f3da309fd92c952bbc870ee28e4391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae1b9bd1a444531ff1e0",
+    "url": "/static/css/main.bd7c78a4.chunk.css"
+  },
+  {
+    "revision": "c4cf013556aea465491e",
+    "url": "/static/js/2.b966b117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b966b117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1b9bd1a444531ff1e0",
+    "url": "/static/js/main.64652ece.chunk.js"
+  },
+  {
+    "revision": "822741c3dcc2c5eee2c5",
+    "url": "/static/js/runtime-main.a17ee0f4.js"
+  }
+]);
---
+++ notes-app/build-old/precache-manifest.8af502e907d950f586208637392fe468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf91f4d10490e5b88eb30269a6596e89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "867144c60ed54c3431d9",
+    "url": "/static/css/main.bd7c78a4.chunk.css"
+  },
+  {
+    "revision": "c4cf013556aea465491e",
+    "url": "/static/js/2.b966b117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b966b117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867144c60ed54c3431d9",
+    "url": "/static/js/main.b072044b.chunk.js"
+  },
+  {
+    "revision": "822741c3dcc2c5eee2c5",
+    "url": "/static/js/runtime-main.a17ee0f4.js"
+  }
+]);
